2238e0d18bdf88a5f3f23a764f365d6024c03402
elisa <elisa@DESKTOP-NOHK2QH>
2022-09-22 11:45:04 -0700

    Add project files.

---
+++ Activity 4 CST-150/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Activity_4_CST_150
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(646, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome. Please enter the number of seconds using the input box below.";
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtSeconds.Location = new System.Drawing.Point(133, 101);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(251, 30);
+            this.txtSeconds.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtOutput.Location = new System.Drawing.Point(133, 151);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(251, 30);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(37, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(48, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnConvert.Location = new System.Drawing.Point(37, 222);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(127, 43);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnClear.Location = new System.Drawing.Point(170, 222);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(127, 43);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 300);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Activity 4 - Grand Canyon University";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Activity 4 CST-150/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_4_CST_150
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            /* First, we declare our variables. 
+             * A double type can represent fractional as well as whole values. 
+             * It can contain up to 15 digits in total, including those before and after the decimal point.
+             * Because our program has a heavy reliance on division, it is wiser to use doubles rather than
+             integers. We want to ensure we round our results properly to avoid needlessly long lines of numbers.*/
+            double seconds; double minutes; double hours; double days; 
+            /* This line will allow us to get our text from the txtSeconds textbox and convert that text into a double for our
+             variable, seconds.*/
+            seconds = double.Parse(txtSeconds.Text);
+            // If the number of seconds the user inputs is less than or equal to 59, we output this string.
+            if (seconds <= 59)
+            {
+                txtOutput.Text = Convert.ToString(seconds + " is less than 60 seconds.");
+            }
+            // If the number of seconds the user inputs is less than or equal to 60 and is less than 3600 seconds, we output this string.
+            else if (seconds >= 60 && seconds < 3600)
+            {
+                minutes = seconds / 60.0;
+                txtOutput.Text = Convert.ToString(seconds + " is " + (Math.Round(minutes, 2) + " minute(s)."));
+            }
+            // If the number of seconds the user inputs is less than or equal to 3600 and is less than 86400 seconds, we output this string.
+            else if (seconds >= 3600 && seconds < 86400)
+            {
+                hours = seconds / 3600.0;
+                txtOutput.Text = Convert.ToString(seconds + " is " + (Math.Round(hours, 2) + " hour(s)."));
+            }
+            // If the number of seconds the user inputs is less than or equal to 86400, we output this string.
+            else if (seconds >= 86400)
+            {
+                days = seconds / 86400.0;
+                txtOutput.Text = Convert.ToString(seconds + " is " + (Math.Round(days, 2) + " day(s)."));
+            }
+            }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // This will clear all of our text when btnClear is clicked. 
+            txtSeconds.Text = "";
+            txtOutput.Text = "";
+            txtSeconds.Focus();
+        }
+    }
+}
